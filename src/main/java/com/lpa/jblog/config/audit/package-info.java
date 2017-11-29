8a2b77b8af95e709e48950f9a365cbae9d09fe0e
L.P <parisot.laurent@gmail.com>
2017-11-29 11:18:01 +0100

    first commit

---
+++ src/main/java/com/lpa/jblog/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.lpa.jblog.config.audit;
---
+++ src/main/java/com/lpa/jblog/repository/AuthorityRepository.java
@@ -0,0 +1,11 @@
+package com.lpa.jblog.repository;
+
+import com.lpa.jblog.domain.Authority;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Spring Data JPA repository for the Authority entity.
+ */
+public interface AuthorityRepository extends JpaRepository<Authority, String> {
+}
